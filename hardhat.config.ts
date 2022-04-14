fd0dc3d2ef05f8c1c9b2ce659d6580f169587c7f
smileman926 <smileman926@hotmail.com>
2022-04-14 15:14:37 +0430

    reset

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,6 @@ import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
